f2ac1c77ff370ba84719b96e68f84961c0b3bff5
vincent1rookie <vincentinfdu@hotmail.com>
2021-11-08 23:43:08 -0500

    add feat: RE + NER + GPT support; change code structure; rewrite several func

--- omneval/tasks/__init__.py
+++ omneval/tasks/__init__.py
@@ -3,34 +3,13 @@ import importlib
 import os
 import torch
 from transformers import AutoTokenizer
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
 from torch.utils.data import DataLoader
 import logging
 from tqdm import tqdm
-import pdb
-import pandas as pd
 import collections
-
-TASK_REGISTRY = {}
-PROCESSOR_REGISTRY = {}
-EVALUATOR_REGISTRY = {}
-METRICS_REGISTRY = {}
-
-
-def collate_fn(batch, exclude=[]):
-    keys = batch[0].keys()
-    return {k: (torch.LongTensor([bz[k] for bz in batch]) if k not in exclude else [bz[k] for bz in batch]) for k in keys}
-
-
-def get_logits(outputs):
-    if hasattr(outputs, 'logits'):
-        logits = outputs.logits
-    elif hasattr(outputs, 'prediction_logits'):
-        logits = outputs.prediction_logits
-    else:
-        raise NotImplementedError
-    return logits
-
+from omneval.utils import collate_fn, get_logits, pad_input_ids, difference, merge_fn
+import pdb
 
 class BaseConfig(object):
     def __init__(self):
@@ -38,6 +17,7 @@ class BaseConfig(object):
         assert getattr(self, 'task_type') is not None, "task type should be specified"
         assert getattr(self, 'metrics') is not None, "evaluation metrics should be specified"
         self.eval_batch_size = getattr(self, 'eval_batch_size', 8)
+        self.metrics_kwargs = getattr(self, 'metrics_kwargs', {})
 
 
 class BaseProcessor(object):
@@ -51,7 +31,7 @@ class BaseProcessor(object):
         self.padding_id = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
         self.mask_token = self.tokenizer.mask_token if self.tokenizer._mask_token is not None else self.tokenizer.unk_token
         self.mask_token_id = self.tokenizer.mask_token_id if self.tokenizer._mask_token is not None else self.tokenizer.unk_token_id
-        self.max_seq_length = getattr(self.config, "max_seq_length", 512)
+        self.max_seq_length = getattr(self.config, "max_seq_length", 64)
 
     def build_dataset(self):
         """Import the raw dataset"""
@@ -89,19 +69,25 @@ class BaseProcessor(object):
         """Prompting each instance and build dataset directly for the Evaluator"""
         prompt_schema = self.config.templates[pid]
         remove_columns = difference(self.raw_data.features.keys(), self.label_name)
-
+        calibrate_word = self.generate_calibrate_example(pid)
+        prompt_length = sum(calibrate_word['attention_mask'])
         return self.raw_data.map(
             lambda x: self.prompting(example=x,
-                                     prompt_schema=prompt_schema),
+                                     prompt_schema=prompt_schema,
+                                     max_length=self.max_seq_length-prompt_length),
                                      remove_columns=remove_columns)
 
-
-    def prompting(self, example, prompt_schema):
+    def prompting(self, example, prompt_schema, max_length=512):
         raise NotImplementedError
 
     def generate_aux_inputs(self, pid=0):
         """Generate other inputs required for the dataset"""
         return {}
+
+    def generate_calibrate_example(self, pid):
+        prompt_schema = self.prompt_schema(pid)
+        null_example = {k: '' for k in self.raw_data.features.keys()}
+        return self.prompting(null_example, prompt_schema)
 
     @property
     def prompt_count(self):
@@ -125,7 +111,7 @@ class BaseEvaluator(object):
         self.arch = config.arch
         self.model = self.build_model()
         self.tokenizer = self.build_tokenizer()
-        self.metrics_fn = build_metrics(config.metrics)
+        self.metrics_fn = load_metric(config.metrics)
         self.padding_id = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
         self.mask_token = self.tokenizer.mask_token if self.tokenizer._mask_token is not None else self.tokenizer.unk_token
         self.mask_token_id = self.tokenizer.mask_token_id if self.tokenizer._mask_token is not None else self.tokenizer.unk_token_id
@@ -169,33 +155,31 @@ class BaseEvaluator(object):
         """
         self.model.eval()
         dataset, kwargs = self.preprocessing(dataset, **kwargs)
-        label_name = getattr(self.config, 'label_name', 'label')
-        dataloader = DataLoader(dataset, batch_size=self.config.eval_batch_size, collate_fn=collate_fn)
+        dataloader = DataLoader(dataset, batch_size=self.config.eval_batch_size, collate_fn=lambda x: collate_fn(x, exclude=[self.label_name]))
         labels = []
         res = collections.defaultdict(list)
         for batch in tqdm(dataloader):
-            label = batch.pop(label_name).view(-1).cpu().detach().tolist()
+            label = batch.pop(self.label_name)
             batch = {k: v.to(self.device) for k, v in batch.items()}
             predictions = self.decode(batch, **kwargs)
             labels += label
             merge_fn(res, predictions)
-        metrics = self.metrics_fn(labels, res['predictions'])
+        metrics = self.metrics_fn.compute(predictions=res['predictions'], references=labels, **self.config.metrics_kwargs)
         res['label'] = labels
-        logging.info("Evaluation metrics of the task %s on model: %s---%s: %.3f" % (
-            self.config.task, self.arch, self.config.metrics, metrics))
         res_list = []
         length = len(res['predictions'])
         for i in range(length):
             res_list.append(self.parse_predictions({k: v[i] for k, v in res.items()}))
-        eval_result = {self.config.metrics: metrics}
+        eval_result = {'plm': self.config.arch}
+        eval_result.update(metrics)
         eval_result.update(self.analysis(res_list))
         return res_list, eval_result
 
-    def write_to_json(self, outputs, output_dir):
-        res = []
-        for output in outputs:
-            res += output
-        with open(output_dir, 'w') as f:
+    def write_inference_to_json(self, res, pid):
+        arch = self.config.arch.split('/')[-1]
+        output_filename = self.config.task+'_'+arch+'_'+str(pid)+'.json'
+        output_filename = os.path.join(self.config.out_dir, output_filename)
+        with open(output_filename, 'w') as f:
             for row in res:
                 f.writelines(str(row)+'\n')
 
@@ -206,122 +190,7 @@ class BaseEvaluator(object):
         raise NotImplementedError
 
 
-def build_config(args):
-    config = TASK_REGISTRY[args.task]()
-    for k, v in vars(args).items():
-        setattr(config, k, v)
-    return config
 
-
-def build_processor(config):
-    return PROCESSOR_REGISTRY[config.task_type](config)
-
-
-def build_evaluator(config):
-    return EVALUATOR_REGISTRY[config.task_type][config.arch](config)
-
-
-def build_metrics(name):
-    return METRICS_REGISTRY[name]
-
-
-def register_task(name):
-    """
-    Configurations of the new task can be added with the :func:`register_task`
-    function decorator.
-
-    For example::
-
-        @register_model('sst2')
-        class SST2Config(BaseConfig):
-            (...)
-    Args:
-        name (str): the name of the model
-    """
-
-    def register_task_cls(cls):
-        if name in TASK_REGISTRY:
-            raise ValueError('Cannot register duplicate task ({})'.format(name))
-        if not issubclass(cls, BaseConfig):
-            raise ValueError('Task ({}: {}) must extend from the BaseConfig'.format(name, cls.__name__))
-        TASK_REGISTRY[name] = cls
-        return cls
-
-    return register_task_cls
-
-
-def register_processor(task_type):
-    """
-    New data processor can be added with the :func:`register_processor`
-    """
-
-    def register_processor_fn(cls):
-        if task_type in PROCESSOR_REGISTRY:
-            raise ValueError('Cannot register duplicate processor ({})'.format(task_type))
-        if not issubclass(cls, BaseProcessor):
-            raise ValueError('Task ({}: {}) must extend from the BaseProcessor'.format(task_type, cls.__name__))
-        PROCESSOR_REGISTRY[task_type] = cls
-        return cls
-
-    return register_processor_fn
-
-
-def register_evaluator(task_type, archs):
-    """
-    New evaluator can be added with the :func:`register_processor`
-    """
-    def register_evaluator_fn(cls):
-        if not issubclass(cls, BaseEvaluator):
-            raise ValueError('Task ({}: {}) must extend from the BaseEvaluator'.format(task_type, cls.__name__))
-        if EVALUATOR_REGISTRY.get(task_type) is None:
-            EVALUATOR_REGISTRY[task_type] = {}
-
-        for arch in archs:
-            if arch in EVALUATOR_REGISTRY[task_type]:
-                raise ValueError('Cannot register duplicate evaluator ({})'.format(arch))
-            EVALUATOR_REGISTRY[task_type][arch] = cls
-        return cls
-
-    return register_evaluator_fn
-
-
-def register_metrics(name):
-    """
-    New metrics can be added with the :func:`register_metrics`
-    """
-    def register_metrics_fn(fn):
-        if name in METRICS_REGISTRY:
-            raise ValueError('Cannot register duplicate metrics ({})'.format(name))
-        if not callable(fn):
-            raise ValueError('metrics must be callable ({})'.format(name))
-        METRICS_REGISTRY[name] = fn
-        return fn
-
-    return register_metrics_fn
-
-
-from sklearn.metrics import accuracy_score, f1_score
-
-@register_metrics('accuracy')
-def accuracy(labels, predictions):
-    return accuracy_score(labels, predictions)
-
-@register_metrics('f1')
-def f1(labels, predictions):
-    return f1_score(labels, predictions)
-
-def merge_fn(dict1, dict2):
-    for k, v in dict2.items():
-        dict1[k] += v
-
-def difference(list1, list2):
-    return [item for item in list1 if item not in list2]
-
-
-# automatically import any Python files in the models/ directory
-for file in os.listdir(os.path.dirname(__file__)):
-    if not file.endswith('.py') and not file.startswith('_'):
-        module = importlib.import_module('tasks.' + file)
 
 
 
