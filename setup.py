b694a6a0ddaba92668199686b939719ff94abd05
vincent1rookie <vincentinfdu@hotmail.com>
2022-03-10 00:49:14 -0500

    modify setup.py to enable console_scripts

--- setup.py
+++ setup.py
@@ -21,9 +21,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ExpressAI/omneval",
     packages=setuptools.find_packages(),
+    install_requires=REQUIRED_PKGS,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'omneval = omneval.main:main'
+        ]}
 )
